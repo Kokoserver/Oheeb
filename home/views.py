e4a0537c90e56fb94db6a18818ac156c16248957
Owonikoko Olaoluwa <48590772+Kokoserver@users.noreply.github.com>
2020-07-02 06:35:40 +0100

    Update views.py

--- home/views.py
+++ home/views.py
@@ -37,7 +37,7 @@ def contact(request):
        messages.success(request,'your form is submitted')
        return render(request, "index.html")
     else:
-       context = {'name':request.POST['name'], 'email':request.POST['email'], 'subject':request.POST['subject'], 'message':request.POST['message']}
+       context = {'name'name, 'email':email, 'subject':subject, 'message':message}
        messages.error(request, 'your form is not submitted')
        return render(request, "contact.html", context)
 
